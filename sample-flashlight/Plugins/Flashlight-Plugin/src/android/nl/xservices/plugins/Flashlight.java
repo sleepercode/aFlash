5c98b0920cdba70d3c61b83a389e5762f05bc440
Zdravko Branzov <Zdravko.Branzov@telerik.local>
2014-06-30 15:25:21 +0300

    Update Flashlight plugin because it didn't work for 3.5

--- sample-flashlight/Plugins/Flashlight-Plugin/src/android/nl/xservices/plugins/Flashlight.java
+++ sample-flashlight/Plugins/Flashlight-Plugin/src/android/nl/xservices/plugins/Flashlight.java
@@ -7,32 +7,33 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import java.util.List;
-
 public class Flashlight extends CordovaPlugin {
 
   private static final String ACTION_AVAILABLE = "available";
   private static final String ACTION_SWITCH_ON = "switchOn";
   private static final String ACTION_SWITCH_OFF = "switchOff";
 
+  private static Boolean capable;
   private Camera mCamera;
-  private Camera.Parameters mParameters;
-
-  public Flashlight() {
-    mCamera = Camera.open();
-  }
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     Log.d("Flashlight", "Plugin Called: " + action);
     if (action.equals(ACTION_SWITCH_ON)) {
+      mCamera = Camera.open();
       toggleTorch(true, callbackContext);
       return true;
     } else if (action.equals(ACTION_SWITCH_OFF)) {
       toggleTorch(false, callbackContext);
+      releaseCamera();
       return true;
     } else if (action.equals(ACTION_AVAILABLE)) {
-      callbackContext.success(isCapable() ? 1 : 0);
+      if (capable == null) {
+        mCamera = Camera.open();
+        capable = isCapable();
+        releaseCamera();
+      }
+      callbackContext.success(capable ? 1 : 0);
       return true;
     } else {
       callbackContext.error("flashlight." + action + " is not a supported function.");
@@ -40,20 +41,32 @@ public class Flashlight extends CordovaPlugin {
     }
   }
 
-  protected boolean isCapable() {
+  private boolean isCapable() {
     return mCamera != null &&
         mCamera.getParameters().getSupportedFlashModes() != null &&
         mCamera.getParameters().getSupportedFlashModes().contains(Camera.Parameters.FLASH_MODE_TORCH);
   }
 
-  protected void toggleTorch(boolean switchOn, CallbackContext callbackContext) {
-    mParameters = mCamera.getParameters();
+  private void toggleTorch(boolean switchOn, CallbackContext callbackContext) {
+    final Camera.Parameters mParameters = mCamera.getParameters();
     if (isCapable()) {
       mParameters.setFlashMode(switchOn ? Camera.Parameters.FLASH_MODE_TORCH : Camera.Parameters.FLASH_MODE_OFF);
       mCamera.setParameters(mParameters);
+      mCamera.startPreview();
       callbackContext.success();
     } else {
       callbackContext.error("Device is not capable of using the flashlight. Please test with flashlight.available()");
     }
+  }
+
+  private void releaseCamera() {
+    // we need to release the camera, so other apps can use it
+    new Thread(new Runnable() {
+      public void run() {
+        mCamera.setPreviewCallback(null);
+        mCamera.stopPreview();
+        mCamera.release();
+      }
+    }).start();
   }
 }
